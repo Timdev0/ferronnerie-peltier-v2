8aa97f60ee89156106142499366cbf490a558663
timdev0 <quentin.peltier.pro@gmail.com>
2025-04-07 10:36:28 +0200

    feat(404): view and router done

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="not-found">
+    <section>
+      <h1>404 - Page introuvable </h1>
+      <p>La page que vous cherchez est introuvable.</p>
+      <router-link aria-label="Retourner à l'accueil" to="/">Retourner à l'accueil</router-link>
+    </section>
+  </div>
+</template>
+
+<style scoped lang="scss">
+section {
+  padding: 2rem 10rem;
+
+  @media screen and (max-width: 768px) {
+    padding: 2rem;
+  }
+}
+</style>
